59b4e72ef3a70e4da7cb7a8d27e077e097dab88a
Antonio Espinoza <aespinozareyes9@gmail.com>
2024-02-03 18:14:00 -0300

    add new login service

--- controllers/auth.js
+++ controllers/auth.js
@@ -36,22 +36,47 @@ const createUser = async (req, res = response) => {
         res.status(500).json({
             ok: false,
             msg: 'Por favor contacte al administrador.'
-        })
-        
+        });
     }
     
 }
 
-const login = (req, res = response) => {
+const login = async (req, res = response) => {
 
     const { email, password} = req.body;
 
-    res.json({
-        ok: true,
-        msg:' login',
-        email,
-        password
-    })
+    try {
+        let user = await User.findOne({email});
+        
+        if(!user) {
+            return res.status(400).json({
+                ok: false,
+                msg: 'El usuario no existe con ese email'
+            })
+        }
+
+        const validPassword = bcrypt.compareSync(password, user.password);
+
+        if(!validPassword){
+            return res.status(400).json({
+                ok: false,
+                msg: 'Password incorrecto'
+            });
+        }
+
+        res.json({
+            ok: true,
+            uid: user.id,
+            name: user.name
+        })
+
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({
+            ok: false,
+            msg: 'Por favor contacte al administrador.'
+        });
+    }
 }
 
 const revalidateToken = (req, res = response) => {
